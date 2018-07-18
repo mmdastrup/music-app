ca844ae77893f3816c67de506180173bad453f84
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-07-18 10:18:33 -0600

    Built reduce functions and counters

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 let defaultStyle = {
   color: "#fff"
 };
+let fakeServerData = {
+  user: {
+    name: "Mike",
+    playlists: [
+      {
+        name: "My favorites",
+        songs: [
+          { name: "Beat It", duration: 1345 },
+          { name: "Cannelloni Makaroni", duration: 1236 },
+          { name: "Rosa helikopter", duration: 70000 }
+        ]
+      },
+      {
+        name: "Top Hits",
+        songs: [
+          { name: "Beat It", duration: 1345 },
+          { name: "Cannelloni Makaroni", duration: 1236 },
+          { name: "Rosa helikopter", duration: 70000 }
+        ]
+      },
+      {
+        name: "Another playlist- the best!",
+        songs: [
+          { name: "Beat It", duration: 135 },
+          { name: "Cannelloni Makaroni", duration: 136 },
+          { name: "Rosa helikopter", duration: 700 }
+        ]
+      },
+      {
+        name: "Playlist - yeah!",
+        songs: [
+          { name: "Beat It", duration: 135 },
+          { name: "Cannelloni Makaroni", duration: 123 },
+          { name: "Rosa helikopter", duration: 700 }
+        ]
+      }
+    ]
+  }
+};
 
-class Aggregate extends Component {
-    render() {
-        return (
-            <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
-                <h2>Number Texts</h2>
-            </div>
-        );
-    }
+class PlaylistCounter extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs);
+    }, []);
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration;
+    }, 0);
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2>{Math.round(totalDuration / 60)} hours</h2>
+      </div>
+    );
+  }
 }
 
 class Filter extends Component {
   render() {
-    return(
-      <div style={{defaultStyle}}>
-        <img/>
-        <input type="text"/>
+    return (
+      <div style={{ defaultStyle }}>
+        <img />
+        <input type="text" />
       </div>
     );
   }
@@ -28,14 +83,14 @@ class Filter extends Component {
 
 class Playlist extends Component {
   render() {
-    return(
-      <div style={{...defaultStyle, width: "25%", display: "inline-block"}}>
+    return (
+      <div style={{ ...defaultStyle, width: "25%", display: "inline-block" }}>
         <img />
         <h3>Playlist Name</h3>
         <ul>
-        <li>Song 1</li>
-        <li>Song 2</li>
-        <li>song 3</li>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>song 3</li>
         </ul>
       </div>
     );
@@ -43,17 +98,34 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { serverData: {} };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData });
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
-        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
-        <Aggregate/>
-        <Aggregate/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
+        {this.state.serverData.user ? (
+          <div>
+            <h1 style={{ ...defaultStyle, "font-size": "54px" }}>
+              {this.state.serverData.user.name}'s Playlists
+            </h1>
+            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
+            <HoursCounter playlists={this.state.serverData.user.playlists}/>
+            <Filter />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+          </div>
+        ) : (
+          <h1 style={{ ...defaultStyle }}>Loading...</h1>
+        )}
       </div>
     );
   }
